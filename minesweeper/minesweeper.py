d448d6f527b973b5d2b21fba3b96c0af46eed0f2
uiandwe <uiandwe@gmail.com>
2016-09-29 22:00:19 +0900

    mine add find mine def

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -14,6 +14,7 @@ class mine_Sweeper:
         self.y = 10
         self.mine = 10
 
+#예외상황 넣기
     def create_map(self):
         for i in range(self.x):
             temp_map_array = []
@@ -21,21 +22,62 @@ class mine_Sweeper:
                 temp_map_array.append("O")
             self.map.append(temp_map_array)
 
+#예외상황 넣기
     def create_mine(self):
         for i in range(self.mine):
+
             mine_x = random.randrange(0, self.x)
             mine_y = random.randrange(0, self.y)
-
+            print(i, mine_x, mine_y)
             self.map[mine_x][mine_y] = 'X'
 
     def view_map(self):
         for i in range(len(self.map)):
             print(self.map[i])
 
+    def find_mine(self):
+        for i in range(self.x):
+            for j in range(self.y):
+                if self.map[i][j] != 'X':
+                    self.map[i][j] = self.check_square(i, j)
+
+    def view_square(self, x, y):
+        square = []
+        for i in range(3):
+            temp_array = []
+            for j in range(3):
+                temp_array.append(self.map[i][j])
+            square.append(temp_array)
+        return square
+
+    def check_square(self, x, y):
+        is_mine = 0
+        if x-1 >= 0 and y-1 >= 0 and self.map[x-1][y-1] == 'X':
+            is_mine += 1
+        if x >= 0 and y-1 >= 0 and self.map[x][y-1] == 'X':
+            is_mine += 1
+        if x+1 < self.x and y-1 >= 0 and self.map[x+1][y-1] == 'X':
+            is_mine += 1
+
+        if x-1 >= 0 and y < self.y and self.map[x-1][y] == 'X':
+            is_mine += 1
+        if x+1 < self.x and y < self.y and self.map[x+1][y] == 'X':
+            is_mine += 1
+
+        if x-1 >= 0 and y+1 < self.y and self.map[x-1][y+1] == 'X':
+            is_mine += 1
+        if x < self.x and y+1 < self.y and self.map[x][y+1] == 'X':
+            is_mine += 1
+        if x+1 < self.x and y+1 < self.y and self.map[x+1][y+1] == 'X':
+            is_mine += 1
+
+        return is_mine
 
 if __name__ == '__main__':
 
     m = mine_Sweeper()
     m.create_map()
     m.create_mine()
+    m.view_map()
+    m.find_mine()
     m.view_map()
--- test/test.py
+++ test/test.py
@@ -1,18 +1,46 @@
 __author__ = 'uiandwe'
 
-
 import pytest
 xfail = pytest.mark.xfail
 
-
 from minesweeper.minesweeper import mine_Sweeper
+
+m = mine_Sweeper()
 
 
 def test_check_create_map():
-    m = mine_Sweeper()
     m.create_map()
-
     assert len(m.map) == 10
+    assert len(m.map[0]) == 10
+
+
+def test_check_mine_count():
+    m.create_mine()
+    count = 0
+    m.view_map()
+    for i in range(m.x):
+        for j in range(m.y):
+            if m.map[i][j] == "X":
+                count += 1
+    assert count == 10
+
+
+# def test_check_mine_square():
+#     map =[['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
+#             ['O', 'O', 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+#             ['O', 'O', 'O', 'O', 'X', 'O', 'O', 'O', 'O', 'O'],
+#             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+#             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+#             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
+#             ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+#             ['O', 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+#             ['O', 'O', 'O', 'O', 'X', 'X', 'O', 'O', 'O', 'O'],
+#             ['O', 'X', 'O', 'O', 'O', 'X', 'O', 'O', 'X', 'O']]
+#     m.map = map
+#     # m.view_map()
+#     m.view_square(1, 8)
+    # count = m.check_square(1, 8)
+    # assert count == 2
 
 
 @xfail
